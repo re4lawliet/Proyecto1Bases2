1d35db1addd8c18714a1fc17b6a08de3e35f46c0
nowell <jorged104@gmail.com>
2019-11-20 17:21:15 -0600

    Compensacion SALIDA TERMINADO

---
+++ src/com/Cheques/BancosCheques.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Cheques;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hp
+ */
+public class BancosCheques {
+    
+    private ArrayList<ICheque> ChequesPorCompensar;
+    
+    public BancosCheques(){
+        ChequesPorCompensar = new ArrayList();
+    }
+    
+ 
+}
